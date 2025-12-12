ada4c7c82407aba8f735df557250304f774465f5
abhishek91soni <abhi83soni@gmail.com>
2025-12-13 04:19:32 +0530

    updates

--- primeNumber.py
+++ primeNumber.py
@@ -4,6 +4,7 @@ if num > 1:
     for i in range(2, num):
         if num % i == 0:
             print(num, "is NOT a prime number")
+            
             break
     else:
         print(num, "is a prime number")
