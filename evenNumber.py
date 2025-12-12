8bc220feb60e36642bd31be60f9dd00d3e958a78
abhishek91soni <abhi83soni@gmail.com>
2025-12-13 03:21:25 +0530

    multiple of two

--- evenNumber.py
+++ evenNumber.py
@@ -1,5 +1,8 @@
 a = int(input("enter the number a : "))
 if a%2 == 0:
     print("The number is Even" ,a)
+    print(a, "is multiple of 2")
 else:
     print("Odd number", a)
+    print(a,"is not the multiple of 2")
+    
